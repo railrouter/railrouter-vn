89a37a6ec9358a647bef674ec7117dfde797f4b8
ngthuc <lenguyenthuc.edu@gmail.com>
2026-05-11 02:26:57 +0700

    Setup Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/railrouter/',
   root: './',
   publicDir: 'public',
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     emptyOutDir: true,
   },
   server: {
